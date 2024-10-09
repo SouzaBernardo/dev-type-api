d309a0992505713496a77dd7a1d49b1ed6400351
bernardo.souza <beportoss@gmail.com>
2024-10-08 21:45:23 -0300

    whitespace

--- src/main/kotlin/com/dev/type/api/service/AIService.kt
+++ src/main/kotlin/com/dev/type/api/service/AIService.kt
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 import java.util.concurrent.ConcurrentMap
 
-
 @Service
 class AIService(
     @Autowired private val aiClient: AIClient,
